2a8a409a1d6a6da8652a7d24eb4b268c67386e1e
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2024-12-02 08:13:56 -0300

    Add support for Firefox

--- popup.js
+++ popup.js
@@ -1,8 +1,10 @@
-document.getElementById('addToCommonsButton').addEventListener('click', function() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+// popup.js
+
+document.getElementById('addToCommonsButton').addEventListener('click', function () {
+  browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
     const url = new URL(tabs[0].url);
     const observationId = url.pathname.split('/')[2];
     const destinationUrl = `https://inat2wiki.toolforge.org/parse/${observationId}`;
-    chrome.tabs.create({ url: destinationUrl });
+    browser.tabs.create({ url: destinationUrl });
   });
 });
